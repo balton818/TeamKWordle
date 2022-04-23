b2930a2179d5a97581e6b0ceee7cf38e24438683
balton818 <balton1@my.westga.edu>
2022-04-23 09:32:43 -0400

    Created functionality to draw guess boxes
    > Programatically generates Boxes and stores them
    in a two-dimensional vector for easy access later.
    > Added a dummy .gitignore to check functionality.

--- view/GameWindow.cpp
+++ view/GameWindow.cpp
@@ -2,9 +2,46 @@
 
 namespace view
 {
+static vector<string> qwertyKeyLabels = {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "ENTER", "BACK"};
+
 GameWindow::GameWindow(int width, int height, const char* title) : Fl_Window(width, height, title)
 {
-    //ctor
+    this->WINDOW_WIDTH = 500;
+    this->WINDOW_HEIGHT = 700;
+
+    this->GUESS_BOXES_START_X = 150;
+    this->GUESS_BOXES_START_Y = 30;
+    this->GUESS_BOX_WIDTH = 30;
+    this->GUESS_BOX_HEIGHT = 40;
+
+    this->ROW_SPACING = 10;
+    this->COLUMN_SPACING = 10;
+
+    this->NUMBER_OF_ROWS = 6;
+    this->NUMBER_OF_COLUMNS = 5;
+
+    this->drawGuessBoxes();
+}
+
+void GameWindow::drawGuessBoxes()
+{
+    int keyCounter = 0;
+
+    for (int rowIndex = 0; rowIndex < this->NUMBER_OF_ROWS; rowIndex++)
+    {
+        vector<Fl_Box*> currentRow;
+        for (int columnIndex = 0; columnIndex < this->NUMBER_OF_COLUMNS; columnIndex++)
+        {
+            int startX = this->GUESS_BOXES_START_X + (columnIndex * this->GUESS_BOX_WIDTH) + (columnIndex * this->COLUMN_SPACING);
+            int startY = this->GUESS_BOXES_START_Y + (rowIndex * this->GUESS_BOX_HEIGHT) + (rowIndex * this->ROW_SPACING);
+            Fl_Box* box = new Fl_Box(startX, startY, 30, 40, "");
+            box->box(FL_DOWN_BOX);
+            box->labelfont(FL_BOLD);
+            box->labelsize(25);
+            currentRow.push_back(box);
+        }
+        this->guessBoxes.push_back(currentRow);
+    }
 }
 
 GameWindow::~GameWindow()
--- view/GameWindow.h
+++ view/GameWindow.h
@@ -2,11 +2,35 @@
 #define GAMEWINDOW_H
 
 #include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+
+#include <vector>
+#include <string>
+using namespace std;
 
 namespace view
 {
 class GameWindow : public Fl_Window
 {
+    private:
+        int WINDOW_WIDTH;
+        int WINDOW_HEIGHT;
+        int GUESS_BOXES_START_X;
+        int GUESS_BOXES_START_Y;
+        int GUESS_BOX_WIDTH;
+        int GUESS_BOX_HEIGHT;
+
+        int ROW_SPACING;
+        int COLUMN_SPACING;
+
+        int NUMBER_OF_ROWS;
+        int NUMBER_OF_COLUMNS;
+
+        static vector<string> qwertyKeyLabels;
+
+        vector<vector<Fl_Box*>> guessBoxes;
+        void drawGuessBoxes();
+
     public:
         GameWindow(int width, int height, const char* title);
         virtual ~GameWindow();
